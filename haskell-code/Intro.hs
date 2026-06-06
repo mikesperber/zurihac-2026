d49dd3cef511678c0a7c94f0beaf6c08c89f5e7e
Mike Sperber <sperber@deinprogramm.de>
2026-06-06 14:22:48 +0200

    Update.

--- haskell-code/Intro.hs
+++ haskell-code/Intro.hs
@@ -36,5 +36,26 @@ isCute pet =
         Cat -> True
         Snake -> False
 -}
+-- template:
+-- isCute Dog = _
+-- isCute Cat = _
+-- isCute Snake = _
 
+isCute Dog = True
+isCute Cat = True
+isCute Snake = False
 
+-- Animals on the Texas highway:
+
+-- Armadillo has following attributes:
+-- - dead or alive   AND
+-- - weight
+-- compound data
+-- record:
+
+data Liveness = Dead | Alive
+
+type Weight = Integer -- type alias
+
+data Dillo = MkDillo { dilloLiveness :: Liveness,
+                       dilloWeight :: Weight }
