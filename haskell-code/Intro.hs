b6b3fe06683dc477d5737910224bd3b278c8b9a0
Mike Sperber <sperber@deinprogramm.de>
2026-06-06 14:13:46 +0200

    Update.

--- haskell-code/Intro.hs
+++ haskell-code/Intro.hs
@@ -1,1 +1,40 @@
 module Intro where
+-- comment
+x :: Integer
+x = 10
+
+y :: Integer
+y = x + 5
+
+-- >>> y
+-- 15
+
+-- data definition:
+-- Pet is one of the following:
+-- - dog  OR
+-- - cat  OR
+-- - snake
+-- case analysis: enumeration
+
+data Pet =
+    Dog 
+  | Cat 
+  | Snake
+
+-- Is a pet cute?
+isCute :: Pet -> Bool
+
+-- >>> isCute Dog
+-- True
+-- >>> isCute Snake
+-- False
+
+{-
+isCute pet =
+    case pet of
+        Dog -> True
+        Cat -> True
+        Snake -> False
+-}
+
+
